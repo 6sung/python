6221ebcf88b59fc58c4cc9dc7af526a876ea795d
6sung <96581415+6sung@users.noreply.github.com>
2022-02-07 15:17:28 +0900

    Add files via upload

---
+++ day01_2.py
@@ -0,0 +1,28 @@
+print(123+123)
+print(120/3)
+
+print('뺄셈 : ',123-30)
+
+print('12 + 54 = ',12+54," 입니다")
+print('268 - 42 = ',268 - 42," 입니다")
+print('2 * 23 = ',2 * 23," 입니다")
+print('120 / 3 = ',120 / 3," 입니다")
+
+num=100
+print("정수 값 : ",num)
+
+num=10+10
+print("변경 후 : ",num)
+
+num=num+10
+print("변경 후 : ",num)
+
+num1=5
+num2=10
+
+sum_ = num1+num2
+print(num1,'+',num2,'=',sum_)
+
+print( id(num1))
+print( id(num2))
+print( id(sum_))
