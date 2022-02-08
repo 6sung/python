e7766889cfbab6b219623219378646493ce95a22
6sung <96581415+6sung@users.noreply.github.com>
2022-02-08 15:05:33 +0900

    Add files via upload

---
+++ day02_2.py
@@ -0,0 +1,21 @@
+# print('수 입력')
+# num1=input()
+# print("입력 받은 수 : ",num1)
+
+# print("\n이름 입력")
+# name=input()
+# print(name,"님의 나이 입력")
+# age=input()
+# print(name,"님의 나이는 ",age,"살 입니다.")
+
+print("두 수 입력")
+num1= input()
+num2 = input()
+num3 = int(num1)+int(num2)
+print("두 수의 합 : ",num3)
+
+print("두 수 입력")
+num1=int(input())
+num2=int(input())
+num3=num1+num2
+print("두 수의 합 : ",num3)
